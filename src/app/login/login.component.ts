7729301fc39492a1d5984ed3ee4ad784855cb2ae
Ahmed Hossam Mustafa <ahmustafa@sumerge.com>
2025-06-02 12:07:37 +0300

    implementing basic login components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginService = inject(LoginService);
+  email: string = '';
+  password: string = '';
 
+  onLogin() {
+    this.loginService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log(response);
+
+      }
+    })
+  }
 }
